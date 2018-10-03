08feada241abf36ad10e888e25de18aed4c85a5b
gurungprakas <gooroongprakas@gmail.com>
2018-10-03 14:07:46 +0545

    first commit

---
+++ src/hoc/Rainbow.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rainbow = (WrappedComponent) => {
+
+    const colours = ['violet', 'indigo', 'blue', 'green', 'yellow', 'red', 'orange']
+    const randomColour = colours[Math.floor(Math.random() *6)]
+    const className = randomColour + '-text';
+
+    return (props) => {
+        return (
+            <div className={className}>
+                <WrappedComponent {...props}/>
+            </div>
+        )
+    }
+}
+
+export default Rainbow;
